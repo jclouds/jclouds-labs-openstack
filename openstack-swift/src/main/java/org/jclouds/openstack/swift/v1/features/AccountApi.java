d0f88e95adfc0e2d860615bad0088922aefa1c37
Everett Toews <everett.toews@rackspace.com>
2014-04-03 17:17:55 -0500

    Better explanation of Beta APIs

--- openstack-swift/src/main/java/org/jclouds/openstack/swift/v1/features/AccountApi.java
+++ openstack-swift/src/main/java/org/jclouds/openstack/swift/v1/features/AccountApi.java
@@ -45,6 +45,10 @@ import org.jclouds.rest.annotations.ResponseParser;
  * <p/>
  * Account metadata prefixed with {@code X-Account-Meta-} will be converted
  * appropriately using a binder/parser.
+ * <p/>
+ * This API is new to jclouds and hence is in Beta. That means we need people to use it and give us feedback. Based
+ * on that feedback, minor changes to the interfaces may happen. This code will replace
+ * org.jclouds.openstack.swift.SwiftClient in jclouds 2.0 and it is recommended you adopt it sooner than later.
  * 
  * @author Jeremy Daggett
  * 
